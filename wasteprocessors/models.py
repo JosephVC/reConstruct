a4f06a8924db83efbe6528d2f784f7bf87462728
robalford <robertsalford@gmail.com>
2016-04-14 10:16:25 -0700

    added __str__ methods to models

--- wasteprocessors/models.py
+++ wasteprocessors/models.py
@@ -6,23 +6,39 @@ from django.contrib.auth.models import User
 class UserType(models.Model):
     user_type = models.CharField(max_length=128) # contractor, home owner, salvage company etc.
 
+    def __str__(self):
+        return self.user_type
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.ForeignKey(UserType)
     company = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.user.username
+
 class ProjectType(models.Model):
     project_type = models.CharField(max_length=128) # new construction, remodel, commercial construction etc.
 
+    def __str__(self):
+        return self.project_type
+
 class Project(models.Model):
     profile = models.ForeignKey(Profile, related_name='projects')
+    project = models.CharField(max_length=128)
     project_type = models.ForeignKey(ProjectType)
     address = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.project
 
 class MaterialType(models.Model):
     material_type = models.CharField(max_length=128) # wood, brick, reusable building materials etc.
     description = models.TextField(blank=True)
     special_considerations = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.material_type
 
 class Material(models.Model):
     material = models.CharField(max_length=128) # pressure treated lumber, cedar siding shingles etc.
@@ -30,13 +46,23 @@ class Material(models.Model):
     description = models.TextField(blank=True)
     special_considerations = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.material
+
 class WasteType(models.Model):
     waste_type = models.CharField(max_length=128) # salvage, scrap, surplus etc.
 
+    def __str__(self):
+        return self.waste_type
+
 class Waste(models.Model):
     project = models.ForeignKey(Project)
+    description = models.CharField(max_length=128)
     waste_type = models.ForeignKey(WasteType)
     material = models.ForeignKey(Material)
+
+    def __str__(self):
+        return self.description
 
 class WasteProcessor(models.Model):
     company = models.CharField(max_length=128)
@@ -49,3 +75,6 @@ class WasteProcessor(models.Model):
     will_pick_up = models.BooleanField()
     will_purchase = models.BooleanField()
     accepts_donations = models.BooleanField()
+
+    def __str__(self):
+        return self.company
