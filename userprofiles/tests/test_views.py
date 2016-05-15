8d5179f56fba5a942dadd78e89c33bd0f7d42bfc
Eric Vegors <tycobass@gmail.com>
2016-05-15 11:07:32 -0700

    test updates

--- userprofiles/tests/test_views.py
+++ userprofiles/tests/test_views.py
@@ -19,84 +19,121 @@ from wasteprocessors.models import \
     Waste, \
     WasteProcessor
 
-
-class SolosBaseTestCase(TestCase):
-
-    def setUp(self):
-        self.factory = RequestFactory()
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.no_funny_hats = Album.objects.create(name='No Funny Hats', slug='no-funny-hats')
-        cls.bugle_call_rag = Track.objects.create(name='Bugle Call Rag', slug='bugle-call-rag', album=cls.no_funny_hats)
-        cls.drum_solo = Solo.objects.create(instrument='drums', artist='Buddy Rich', track=cls.bugle_call_rag,
-                                            slug='buddy-rich')
-
-        cls.giant_steps = Album.objects.create(name='Giant Steps', slug='giant-steps')
-        cls.mr_pc = Track.objects.create(name='Mr. PC', slug='mr-pc', album=cls.giant_steps)
-        cls.sax_solo = Solo.objects.create(instrument='saxophone', artist='John Coltrane', track=cls.mr_pc,
-                                           slug='john-coltrane')
-
-class IndexViewTestCase(SolosBaseTestCase):
-
-    def test_index_view_basic(self):
-        """
-        Test that index view returns a 200 response and uses the correct template
-        """
-        request = self.factory.get('/')
-        response = index(request)
-        with self.assertTemplateUsed('solos/index.html'):
-            response = index(request)
-            self.assertEqual(response.status_code, 200)
-
-    def test_index_view_returns_solos(self):
-        """
-        Test that the index view will attempt to return Solos if query parameters exist
-        """
-        response = self.client.get('/', {'instrument': 'drums'})
-        solos = response.context['solos']
-
-        self.assertIs(type(solos), QuerySet)
-        self.assertEqual(len(solos), 1)
-        self.assertEqual(solos[0].artist, 'Buddy Rich')
-
-    @patch('solos.models.Solo.get_artist_tracks_from_musicbrainz')
-    def test_index_view_returns_external_tracks(self, mock_solos_get_from_mb):
-        """
-        Test that the index view will return artists from the MusicBrainz API if none are returned from our database
-        """
-        mock_solo = Mock()
-        mock_solo.artist = 'Jaco Pastorius'
-        mock_solos_get_from_mb.return_value = [mock_solo]
-
-        response = self.client.get('/', {
-            'instrument': 'Bass',
-            'artist': 'Jaco Pastorius' # not currently in our database
-        })
-
-        solos = response.context['solos']
-        self.assertEqual(len(solos), 1)
-        self.assertEqual(solos[0].artist, 'Jaco Pastorius')
+from django.test import Client
+from userprofiles.models import UserType, Profile
+import datetime
+from django.core.urlresolvers import reverse
+client = Client()
 
 
+class LoginTestCase(TestCase):
 
-class SoloViewTestCase(SolosBaseTestCase):
+    # user signup form
+    def test_new_user(self):
 
-    def setUp(self):
-        self.factory = RequestFactory()
+        response = client.post('login', data={
+        'user': 'tester',
+        'email': 'test01@test.com',
+        'password1': 'letmein123!',
+        'password2': 'letmein123!',
+        }, follow=True)
 
-    def test_basic(self):
-        """
-        Test that the solo view returns a 200 response, uses the correct template, and has the correct context
-        """
-        request = self.factory.get('/solos/no-funny-hats/bugle-call-rag/buddy-rich/')
+        # checking if the user is logged in
+        with self.assertRaises(KeyError):
+            client.session['_auth_user_id']
 
-        with self.assertTemplateUsed('solos/solo_detail.html'):
-            response = solo_detail(request, album=self.no_funny_hats.slug, track=self.bugle_call_rag.slug,
-                                   artist=self.drum_solo.slug)
+        # self.assertEquals(len(mail.outbox), 1)
+        # url = find_verification_url(mail.outbox[0].body)
+        response = client.get(url, follow=True)
+        self.assertEqual(200, response.status_code)
 
-            self.assertEqual(response.status_code, 200)
-            page = response.content.decode()
-            self.assertInHTML('<p id="jmad-artist">Buddy Rich</p>', page)
-            self.assertInHTML('<p id="jmad-track">Bugle Call Rag [1 solo]</p>', page)
+        user = User.objects.get(email="test01@test.com")
+
+        self.assertEqual(client.session['_auth_user_id'], user.pk)
+
+        # how to logout a user?
+        force_logout()
+
+        self.assertFalse(response.request.user.is_authenticated())
+
+
+# class UserProfileBaseTestCase(TestCase):
+
+#     def setUp(self):
+#         self.factory = RequestFactory()
+
+#     @classmethod
+#     def setUpClass(cls):
+#         super().setUpClass()
+#         cls.no_funny_hats = Album.objects.create(name='No Funny Hats', slug='no-funny-hats')
+#         cls.bugle_call_rag = Track.objects.create(name='Bugle Call Rag', slug='bugle-call-rag', album=cls.no_funny_hats)
+#         cls.drum_solo = Solo.objects.create(instrument='drums', artist='Buddy Rich', track=cls.bugle_call_rag,
+#                                             slug='buddy-rich')
+
+#         cls.giant_steps = Album.objects.create(name='Giant Steps', slug='giant-steps')
+#         cls.mr_pc = Track.objects.create(name='Mr. PC', slug='mr-pc', album=cls.giant_steps)
+#         cls.sax_solo = Solo.objects.create(instrument='saxophone', artist='John Coltrane', track=cls.mr_pc,
+#                                            slug='john-coltrane')
+
+# class IndexViewTestCase(SolosBaseTestCase):
+
+#     def test_index_view_basic(self):
+#         """
+#         Test that index view returns a 200 response and uses the correct template
+#         """
+#         request = self.factory.get('/')
+#         response = index(request)
+#         with self.assertTemplateUsed('solos/index.html'):
+#             response = index(request)
+#             self.assertEqual(response.status_code, 200)
+
+#     def test_index_view_returns_solos(self):
+#         """
+#         Test that the index view will attempt to return Solos if query parameters exist
+#         """
+#         response = self.client.get('/', {'instrument': 'drums'})
+#         solos = response.context['solos']
+
+#         self.assertIs(type(solos), QuerySet)
+#         self.assertEqual(len(solos), 1)
+#         self.assertEqual(solos[0].artist, 'Buddy Rich')
+
+#     @patch('solos.models.Solo.get_artist_tracks_from_musicbrainz')
+#     def test_index_view_returns_external_tracks(self, mock_solos_get_from_mb):
+#         """
+#         Test that the index view will return artists from the MusicBrainz API if none are returned from our database
+#         """
+#         mock_solo = Mock()
+#         mock_solo.artist = 'Jaco Pastorius'
+#         mock_solos_get_from_mb.return_value = [mock_solo]
+
+#         response = self.client.get('/', {
+#             'instrument': 'Bass',
+#             'artist': 'Jaco Pastorius' # not currently in our database
+#         })
+
+#         solos = response.context['solos']
+#         self.assertEqual(len(solos), 1)
+#         self.assertEqual(solos[0].artist, 'Jaco Pastorius')
+
+
+
+# class SoloViewTestCase(SolosBaseTestCase):
+
+#     def setUp(self):
+#         self.factory = RequestFactory()
+
+#     def test_basic(self):
+#         """
+#         Test that the solo view returns a 200 response, uses the correct template, and has the correct context
+#         """
+#         request = self.factory.get('/solos/no-funny-hats/bugle-call-rag/buddy-rich/')
+
+#         with self.assertTemplateUsed('solos/solo_detail.html'):
+#             response = solo_detail(request, album=self.no_funny_hats.slug, track=self.bugle_call_rag.slug,
+#                                    artist=self.drum_solo.slug)
+
+#             self.assertEqual(response.status_code, 200)
+#             page = response.content.decode()
+#             self.assertInHTML('<p id="jmad-artist">Buddy Rich</p>', page)
+#             self.assertInHTML('<p id="jmad-track">Bugle Call Rag [1 solo]</p>', page)
--- wasteprocessors/tests/test_urls.py
+++ wasteprocessors/tests/test_urls.py
@@ -22,22 +22,13 @@ class WasteprocessorsURLsTestCase(TestCase):
         Test that the /profile of the site resolves to the 
         profile_view function
         """
-        m_search = resolve('/project/P<project_slug>-a/')
+        m_search = resolve('/project/laurelhurst-house/')
         self.assertEqual(m_search.func, material_search_view)
 
-
-    def test_waste_id_url_uses_material_search_results_A(self):
+    def test_waste_id_url_uses_material_search_results(self):
         """
         Test that the waste_id (regex) of the site resolves to the 
         material_search_results function
         """
-        material_search_res = resolve('material-search/results/1/')
-        self.assertEqual(material_search_res.func, material_search_results)
-
-    def test_waste_id_url_uses_material_search_results_B(self):
-        """
-        Test that the waste_id (regex) of the site resolves to the 
-        material_search_results function
-        """
-        material_search_res = resolve('material-search/results/P<waste_id>2/')
+        material_search_res = resolve('/material-search/results/1/')
         self.assertEqual(material_search_res.func, material_search_results)
