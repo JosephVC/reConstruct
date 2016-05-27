c497a21ab7143cea2960bb960e26600324f86147
JosephVC <josephvcardenas@gmail.com>
2016-05-26 17:48:48 -0700

    Commit of git hub project documentation

--- mysite/production.py
+++
@@ -1,5 +0,0 @@
-from mysite.settings import *
-
-DEBUG = False
-ALLOWED_HOSTS = ['ec2-52-37-97-228.us-west-2.compute.amazonaws.com', 'localhost']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- wasteprocessors/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WasteprocessorsConfig(AppConfig):
-    name = 'wasteprocessors'
